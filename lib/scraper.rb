c4a1a443e93651cb082e27f40d6f939463a91be8
Lmilne <lmilne360@gmail.com>
2016-12-07 00:21:00 -0500

    Finished Scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,19 @@ class Scraper
 	@@site = 'http://www.deviantart.com/browse/all'
 
 	def self.scrape(url = @@site)
+		art_pieces = []
 		index = Nokogiri::HTML(open(url))
-		
+		all = index.css(".thumb") # container of all art pieces
+		all.each do |item|
+			
+			title = item.css(".title").text
+			artist = item.css(".artist").text
+			link = item["href"]
+
+			art_pieces << {title: title, artist_name: artist, link: link}
+		end
+
+		art_pieces
 	end
 end
 =begin
