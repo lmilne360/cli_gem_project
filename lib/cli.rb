09875ea6631469f234f175540708957e8ce0f90d
Lmilne <lmilne360@gmail.com>
2016-12-07 19:22:31 -0500

    Updated Cli start

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,22 +1,32 @@
 class Cli
 
-data = Scraper.scrape
+@@site = 'http://www.deviantart.com/browse/all' #default site
 
-data.each do |art_data|
+	def self.start
+		puts " Welcome to the art browser, would you like to view the newest, popular, or undiscovered  art pieces?"
+		choice = gets.strip
+
+		if choice.downcase == 'newest'
+			@@site == 'http://www.deviantart.com/browse/all/digitalart/?order=5'
+		elsif choice.downcase == 'popular'
+			@@site = 'http://www.deviantart.com/browse/all/digitalart/?order=11'
+		elsif choice.downcase == 'undiscovered'
+			@@site == 'http://www.deviantart.com/browse/all/digitalart/?order=134217728'
+		else
+		puts "that's not an option, viewing default page"		
+		end
+
+	end
+
+
+		@data = Scraper.scrape(@@site)	
+
+
+
+@data.each do |art_data|
 	artist = Artist.new(art_data)
 	art = Art.new(art_data)
 	art.artist = artist	
-	#binding.pry
-end
-
-def self.start
-	puts "Welcome! What Would you like to do?"
-	answer = gets.strip.downcase
-
-	case answer
-	when "list art"
-		list_art
-	end
 end
 
 
@@ -24,5 +34,8 @@ def self.list_art
 	Art.all.each.with_index(1) {|art, i| puts "#{i}. #{art.title} by #{art.artist.name}"}
 end
 
+def self.list_artists
+	Artist.all.each.with_index(1) {|artist, i| puts "#{i}. #{artist.name} Page: #{artist.page}"}
+end
 
 end
