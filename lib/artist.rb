d9988e05d372c3837fd261a8aa6f1940722cdfc2
Lmilne <lmilne360@gmail.com>
2016-12-07 22:21:50 -0500

    Set comments, pagevies, art_pieces for Artist

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,7 @@
 class Artist
 	extend Abstractable::ClassMethods
 	include Abstractable::InstanceMethods
- 	attr_accessor :name, :page
- 	attr_reader :art_pieces :comments, :pageviews
+ 	attr_accessor :name, :page, :art_pieces :comments, :pageviews
 
  @@all = []
 
@@ -22,10 +21,15 @@ class Artist
 
  def scrape_profile(@page)
  	doc = Nokogiri:HTML(open(@page))
- 	details = doc.css("span.tight").text.split("\n").map do |i|
+ 	details = doc.css("span.tight").text.split("\n").map do |i| #details is an array containing [1] amount art pieces [2]  total comments [3] total pageviews 
  		i.strip
  	end.delete_if(&:empty?)
+
+ 	self.art_pieces = details[1]
+ 	self.comments = details[2]
+ 	self.pageviews = details[3]
  end
 
 
 end
+
