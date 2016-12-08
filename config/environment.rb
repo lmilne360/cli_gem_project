703ac47e108b1381e5fb9420d858ccf7137d816c
Lmilne <lmilne360@gmail.com>
2016-12-08 00:47:19 -0500

    updated spec

---
+++ config/environment.rb
@@ -0,0 +1,9 @@
+require 'bundler'
+
+Bundler.require
+
+require 'require_all'
+require 'pry'
+require 'nokogiri'
+
+require_all 'lib'
---
+++ lib/models/art.rb
@@ -0,0 +1,29 @@
+class Art
+	attr_accessor :title, :artist, :link
+
+	@@all = []
+
+	def initialize(art_data)
+		@title = art_data[:title]
+		@link = art_data[:link]
+		save
+	end
+
+	def save
+ 	self.class.all << self
+ 	end
+
+	def self.all
+		@@all
+	end
+
+	def self.find_by_title(title)
+		self.all.detect{|art| art.title = title}
+	end
+
+	def self.find_by_artist(artist_name)
+		self.all.detect{|art| art.artist.name = artist_name}
+	end
+
+
+end
